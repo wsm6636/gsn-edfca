85a0436b586608a3c2e03287dfe84c4257a8d659
wsm6636 <812609965@qq.com>
2022-04-01 13:56:19 +0800

    EDFCA,EDFCA2,FPCA add lock_cache_way

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 33 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Mar 8 16:27:11 CST 2022"
+#define UTS_VERSION "#33 SMP PREEMPT Thu Mar 31 20:53:16 CST 2022"
 #define LINUX_COMPILE_BY "wsm"
-#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_HOST "wsm-Z11PA-D8-Series"
 #define LINUX_COMPILER "gcc version 5.3.0 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.1.15-litmus-fp-bw"
+#define UTS_RELEASE "4.1.15-test-gdb-gd27b1520-dirty"
